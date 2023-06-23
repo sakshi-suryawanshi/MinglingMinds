d20a2793164f233d73a5d531c69dcdbf591517e6
sakshi-suryawanshi1 <sakshisuryawanshi1234@gmail.com>
2023-06-23 15:56:20 +0530

    start

---
+++ mingling_minds/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mingling_minds project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mingling_minds.settings')
+
+application = get_wsgi_application()
