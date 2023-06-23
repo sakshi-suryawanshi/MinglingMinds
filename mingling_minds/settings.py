13018ccfa79e2d04970023aa41b28381d3d4f68c
sakshi-suryawanshi1 <sakshisuryawanshi1234@gmail.com>
2023-06-23 17:33:12 +0530

    render html

--- mingling_minds/settings.py
+++ mingling_minds/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mingling_minds.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
