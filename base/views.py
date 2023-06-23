63ff54e68002afe9357c2e1dfeefc63d826003de
sakshi-suryawanshi1 <sakshisuryawanshi1234@gmail.com>
2023-06-23 18:57:15 +0530

    template engine

--- base/views.py
+++ base/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id':1, "name":"Geo Politics"},
+    {'id':2, "name":"Finance"},
+    {'id':3, "name":"Real Estate"},
+    {'id':4, "name":"Education"}
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms':rooms}
+    return render(request, 'home.html', context)
 
 def room(request):
     return render(request, 'room.html')
