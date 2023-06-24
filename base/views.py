f0ee68dce39a20d60b1eb09c089e0a4ef415772c
sakshi-suryawanshi1 <sakshisuryawanshi1234@gmail.com>
2023-06-24 09:39:32 +0530

    entering to room

--- base/views.py
+++ base/views.py
@@ -11,7 +11,13 @@ rooms = [
 
 def home(request):
     context = {'rooms':rooms}
-    return render(request, 'home.html', context)
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for r in rooms:
+        if int(pk) == r['id']:
+            room = r
+            break
+    context = {'room':room}
+    return render(request, 'base/room.html', context)
