2038bd9738c66d5d772cfc87f1c3775dbc05e063
sakshi-suryawanshi1 <sakshisuryawanshi1234@gmail.com>
2023-06-28 16:20:12 +0530

    register

--- base/views.py
+++ base/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from .models import Room, Topic, User
 from .forms import RoomForm
@@ -18,30 +19,51 @@ from .forms import RoomForm
 
 def loginPage(request):
 
-    if request.user.is_authenticated:
-        return redirect('home')
+    page = 'login'
 
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    # try:
-    #     user = User.objects.get(username=username)
-    # except:
-    #     messages.error(request, 'User does not exists.')
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            return redirect('home')
 
-    user = authenticate(request, username=username, password=password)
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        # try:
+        #     user = User.objects.get(username=username)
+        # except:
+        #     messages.error(request, 'User does not exists.')
 
-    if user is not None:
-        login(request, user)
-        return redirect('home')
-    else:
-        messages.error(request, 'Username OR password does not exist')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist')
      
-    context = {}
+    context = {'page':page}
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    page = 'register'
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+             messages.error(request, 'An error occured during registration!!')
+
+    context = {'form':form,'page':page}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
